6a40fb7dde7f6be57915964fea7770d79b131f4e
Mateusz Delegacz <deli@Mateuszs-iMac.local>
2020-06-12 18:38:40 +0100

    1.2 finished

--- courseinfo/courseinfo/src/index.js
+++ courseinfo/courseinfo/src/index.js
@@ -29,15 +29,9 @@ const Header = (props) => {
 const Content = (props) => {
     return (
       <>
-        <p>
-          {props.p1} {props.e1}
-        </p>
-        <p>
-          {props.p2} {props.e2}
-        </p>
-        <p>
-          {props.p3} {props.e3}
-        </p>
+        <Part part={props.p1} exercise={props.e1}/>
+        <Part part={props.p2} exercise={props.e2}/>
+        <Part part={props.p3} exercise={props.e3}/>
       </>
     )
 }
@@ -45,6 +39,15 @@ const Total = (props) => {
   return (
     <>
       <p>Number of exercises {props.e1+props.e2+props.e3}</p>
+    </>
+  )
+}
+const Part = (props) => {
+  return (
+    <>
+    <p>
+      {props.part} {props.exercise}
+    </p>
     </>
   )
 }
